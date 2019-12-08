8780e9c1b482706e53f214fdc096503c9c5c48df
AtharvaUmbarkar <57295061+AtharvaUmbarkar@users.noreply.github.com>
2019-12-09 03:54:45 +0530

    Corrected Code

--- 2.cpp
+++ 2.cpp
@@ -9,20 +9,42 @@ using namespace std;
 int main(){
     char s[100];
     cin>>s;
-    int l=strlen(s),a[26],b=0,c=0;
+    int d=0,t=0;
+    int l=strlen(s),a[26];
     for(int i=0;i<26;i++){
         a[i]=0;
     }
     for(int i=0;i<l;i++){
         a[s[i]-97]+=1;
     }
-    for(int i=0;i<l-1;i++){
+    for(int i=0;i<26;i++){
+        if(a[i]!=0) t+=1;
+    }
+    int r[t];t=0;
+    for(int i=0;i<26;i++){
+        if(a[i]!=0){r[t]=a[i];t+=1;}
+    }
+    /*for(int i=0;i<l-1;i++){
         if(a[s[i]-97]==a[s[i+1]-97]) b+=1;
         if((a[s[i]-97]!=a[s[i+1]-97])&&(abs(a[s[i]-97]-a[s[i+1]-97])==1)) c+=1;
+        if(c>1) d+=1;
     }
     if(b==l-1) printf("YES");
-    else if(c==1) printf("YES");
-    else printf("NO");
+    else if((c==1)&&(d==0)) printf("YES");
+    else printf("NO");*/
+    int flag1=0,flag2=0;
+    for(int i=0;i<t;i++){
+        d=0;
+        for(int j=0;j<t;j++){
+            if(r[i]==r[j]){
+                d+=1;
+            }
+            else if(abs(r[i]-r[j])>1) flag2=1;
+        }
+        if((d==t)||(d==t-1)&&(flag2!=1)) flag1=1;
+    }
+    if(flag1) cout<<"YES";
+    else cout<<"NO";
 
     return 0;
 }
--- matrix.cpp
+++ matrix.cpp
@@ -30,6 +30,11 @@ int main(){
                     b[n-1-i][j]=a[j][i];
                 }
             }
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    a[i][j]=b[i][j];
+                }
+            }
         }
     }
     if(r<0){
@@ -38,6 +43,11 @@ int main(){
             for(int i=0;i<n;i++){
                 for(int j=n-1;j>=0;j--){
                     b[i][n-1-j]=a[j][i];
+                }
+            }
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    a[i][j]=b[i][j];
                 }
             }
         }
@@ -59,21 +69,3 @@ int main(){
 
     return 0;
 }
-/*Motivational Quotes of the Day
-1. "People often say that motivation doesn't last. Well, neither does bathing -- that's why we recommend it daily." -Zig Ziglar
-
-2. "Someday is not a day of the week." -Denise Brennan-Nelson
-
-3. "Hire character. Train skill." -Peter Schutz
-
-4. "Your time is limited, so don't waste it living someone else's life." -Steve Jobs
-
-5. "Sales are contingent upon the attitude of the salesman -- not the attitude of the prospect." -W. Clement Stone
-
-6. "Everyone lives by selling something." -Robert Louis Stevenson
-
-7. "If you are not taking care of your customer, your competitor will." -Bob Hooey
-
-8. "The golden rule for every businessman is this: Put yourself in your customer's place." -Orison Swett Marden
-
-9. "If you cannot do great things, do small things in a great way." -Napoleon Hill*/
